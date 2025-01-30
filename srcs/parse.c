20a94429780f2f3cc591a505b8b06c338b8348a4
Dimitri Feve <dfeve@c3r2p6.42nice.fr>
2025-01-30 22:30:20 +0100

    fixed everything

--- srcs/parse.c
+++ srcs/parse.c
@@ -6,7 +6,7 @@
 /*   By: dfeve <dfeve@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/07 05:23:02 by dfeve             #+#    #+#             */
-/*   Updated: 2025/01/27 18:32:49 by dfeve            ###   ########.fr       */
+/*   Updated: 2025/01/30 22:29:44 by dfeve            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,12 +52,12 @@ t_pile	*parse_str(char *str)
 	i = 0;
 	while (split[i])
 	{
-		if ((split[i][0] == '-' || split[i][0] == '+') && !split[i][1])
+		if (((split[i][0] == '-' || split[i][0] == '+') && !split[i][1]) || check_num(split[i]) == -1)
 			error("Need a number as argument", result);
+		if (ft_atoi(split[i]) > INT_MAX || ft_atoi(split[i]) < INT_MIN || ft_strlen((const char *)split[i]) >= 20)
+			error("Number not in range", result);
 		if (check_pile(ft_atoi(split[i]), result) == -1)
 			error("numbers have to be different from one another", result);
-		if (ft_atoi(split[i]) > INT_MAX || ft_atoi(split[i]) < INT_MIN)
-			error("Number not in range", result);
 		pile_add_back(&result, new_pile(ft_atoi(split[i])));
 		i++;
 	}
@@ -77,10 +77,10 @@ t_pile	*parse_opt(char **opt)
 		if (((opt[i][0] == '-' || opt[i][0] == '+')
 			&& !opt[i][1]) || check_num(opt[i]) == -1)
 			error("Need a number as argument", result);
+		if (ft_atoi(opt[i]) > INT_MAX || ft_atoi(opt[i]) < INT_MIN || ft_strlen((const char *)opt[i]) >= 20)
+			error("Number not in range", result);
 		if (check_pile(ft_atoi(opt[i]), result) == -1)
 			error("numbers have to be different from one another", result);
-		if (ft_atoi(opt[i]) > INT_MAX || ft_atoi(opt[i]) < INT_MIN)
-			error("Number not in range", result);
 		pile_add_back(&result, new_pile(ft_atoi(opt[i])));
 		i++;
 	}
